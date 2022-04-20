446c003aa9271eff857b60d3048353dcf7d4c441
Tomás Ferreira <tomasferreira91@gmail.com>
2022-04-20 20:17:33 +0100

    Troca de ecrã ao selecionar um novo icon na BottomNavBar

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,83 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:src/screens/alerts_map_screen.dart';
-import 'package:src/screens/alerts_screen.dart';
-import 'package:src/screens/home.dart';
 
-const userId = 1;
+import 'package:src/screens/home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final screens = [
-    Home(),
-    Alerta(),
-    mapScreenAlert(),
-  ];
-
-  int _selectedIndex = 2;
-
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Alertas',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Search',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Profile',
-      style: optionStyle,
-    ),
-  ];
-
   MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Página Principal',
       theme: ThemeData.dark(),
-      home: Scaffold(
-        body: screens[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-              backgroundColor: Colors.red,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.warning),
-              label: 'Alertas',
-              backgroundColor: Colors.green,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.manage_search),
-              label: 'Search',
-              backgroundColor: Colors.pink,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.supervised_user_circle),
-              label: 'Profile',
-              backgroundColor: Colors.purple,
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.amber[800],
-          onTap: (index) => _selectedIndex = index,
-        ),
-      ),
-    );
+      home: Home());
   }
 }
--- src/lib/screens/home.dart
+++ src/lib/screens/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'alerts_map_screen.dart';
+import 'alerts_screen.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -8,12 +11,73 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final screens = [
+    Alerta(),
+    mapScreenAlert(),
+  ];
+
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Alertas',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Search',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Profile',
+      style: optionStyle,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
-        body: Text("ola"),
+        appBar:AppBar(
+            title:const Text("ASA")),
+        body: screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.red,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.warning),
+              label: 'Alertas',
+              backgroundColor: Colors.green,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.manage_search),
+              label: 'Search',
+              backgroundColor: Colors.pink,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle),
+              label: 'Profile',
+              backgroundColor: Colors.purple,
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: (index){_selectedIndex = index;
+            setState(() {
+            });
+          }
+          ),
       ),
     );
   }
