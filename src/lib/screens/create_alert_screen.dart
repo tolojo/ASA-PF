fd7f2b44bb0a8ff950de573488ecc2c9b4f3f9d9
jptr0508 <jptorgalrolo@gmail.com>
2022-06-02 17:52:49 +0100

    Alertas

---
+++ src/lib/screens/create_alert_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CreateAlert extends StatefulWidget {
+  CreateAlert({Key? key}) : super(key: key);
+
+  @override
+  State<CreateAlert> createState() => _CreateAlertState();
+}
+
+class _CreateAlertState extends State<CreateAlert> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- src/lib/screens/home_screen.dart
+++ src/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'create_PsaName_screen.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -15,6 +17,15 @@ class _HomeScreenState extends State<HomeScreen> {
           title: const Text("ASA Homepage"),
           centerTitle: true,
           backgroundColor: const Color(0xFF9DD6E2),),
+          floatingActionButton: FloatingActionButton(
+          onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => CreatePsaName()));
+        },
+        backgroundColor: const Color(0xFF9DD6E2),
+        child: const Icon(Icons.add),
+      ),
     );
+    body: ;
   }
 }
