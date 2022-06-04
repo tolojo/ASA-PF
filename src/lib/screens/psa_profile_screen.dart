e1ed49f15a5770f2dcdd935a721ad905d5ef874d
jptr0508 <jptorgalrolo@gmail.com>
2022-06-04 16:01:46 +0100

    Search Funcional

---
+++ src/lib/screens/psa_profile_screen.dart
@@ -0,0 +1,147 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PSAProfile extends StatefulWidget {
+  const PSAProfile({Key? key}) : super(key: key);
+
+  @override
+  State<PSAProfile> createState() => _PSAProfileState();
+}
+
+class _PSAProfileState extends State<PSAProfile> {
+  final url = "http://10.0.2.2:3000/cidadao/";
+  var _cidadaoJson;
+  final bool _isLoading = false;
+
+  Future<void> fetchPSACarac() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getString('id');
+    print(userId);
+    try {
+      final response = await http.get(Uri.parse(url + userId!));
+      final jsonData = jsonDecode(response.body);
+      setState(() {
+        _cidadaoJson = jsonData;
+      });
+      _cidadaoJson = jsonData;
+    } catch (err) {}
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPSACarac();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final post = _cidadaoJson;
+    if (post != null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("ASA Profile"),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF9DD6E2),
+        ),
+        body: _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView(
+                children: <Widget>[
+                  Container(
+                    color: const Color(0xFFCEE9F3),
+                    height: 250,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: const <Widget>[
+                            CircleAvatar(
+                              minRadius: 75.0,
+                              child: CircleAvatar(
+                                radius: 75.0,
+                                backgroundImage: AssetImage(
+                                    "assets/images/Group 193profilepic.jpg"),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "${post["cidadao_nome"]}",
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontFamily: 'Quicksand',
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF537597),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: const <Widget>[
+                      ListTile(
+                        title: Text(
+                          'Editar Perfil',
+                          style: TextStyle(
+                              color: Color(0xFF537597),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Quicksand'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
+                  Column(
+                    children: const <Widget>[
+                      ListTile(
+                        title: Text(
+                          'Definições',
+                          style: TextStyle(
+                              color: Color(0xFF537597),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Quicksand'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
+                  Column(
+                    children: const <Widget>[
+                      ListTile(
+                        title: Text(
+                          'Terminar Sessão',
+                          style: TextStyle(
+                              color: Color(0xFF537597),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Quicksand'),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          leading: const BackButton(),
+          title: const Text("Create PSA profile"),
+          centerTitle: true,
+        ),
+        body: const Center(child: Text("loading")),
+      );
+    }
+  }
+}
--- src/lib/screens/search_screen.dart
+++ src/lib/screens/search_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+import 'package:src/screens/psa_profile_screen.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -11,25 +12,130 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  final url = "http://10.0.2.2:3000/psa/";
-  var _psaJson;
+  final formkey = GlobalKey<FormState>();
+  final url = "http://10.0.2.2:3000/psa/nome/";
+  var _psaJson = null;
+  TextEditingController searchController = TextEditingController();
 
-  Future<void> fetchCidadao() async {
+  Future<void> fetchPSA(String nome) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    final userId = prefs.getString('id');
-    print(userId);
     try {
-      final response = await http.get(Uri.parse(url + userId!));
+      final response = await http.get(Uri.parse(url + searchController.text));
       final jsonData = jsonDecode(response.body);
       setState(() {
         _psaJson = jsonData;
       });
-      _psaJson = jsonData;
+      print(_psaJson);
     } catch (err) {}
   }
 
   @override
   Widget build(BuildContext context) {
+    while (_psaJson != null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("ASA Search"),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF9DD6E2),
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 0, vertical: 16),
+                child: TextField(
+                  key: formkey,
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search,
+                        color: Color(0xFF9DD6E2), size: 30),
+                    contentPadding: const EdgeInsets.symmetric(vertical: 30.0),
+                    fillColor: const Color.fromARGB(255, 223, 223, 223),
+                    filled: true,
+                    hintText: "Pesquise Aqui",
+                    hintStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        fontFamily: 'Quicksand',
+                        color: Color(0xFF656565)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: const BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: _psaJson.length,
+                itemBuilder: (context, i) {
+                  final post = _psaJson[i];
+                  return Card(
+                    color: const Color.fromARGB(255, 223, 223, 223),
+                    elevation: 15,
+                    shadowColor: Colors.grey,
+                    child: ListTile(
+                      onTap: (() {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (_) => const PSAProfile(),
+                        ));
+                      }),
+                      title: Text(
+                        "${post["psa_nome_provisorio"]}",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            fontFamily: 'Quicksand',
+                            color: Color(0xFF537597)),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Material(
+                elevation: 15,
+                shadowColor: Colors.grey,
+                borderRadius: BorderRadius.circular(15),
+                child: ElevatedButton(
+                  onPressed: () {
+                    fetchPSA(searchController.text);
+                    if (formkey.currentState!.validate()) {}
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(150, 20, 150, 20),
+                      primary: const Color(0xFF77BECE),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                  child: const Text(
+                    'Pesquisar',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Roboto',
+                        fontSize: 20,
+                        color: Color(0xFFFFFFFF)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("ASA Search"),
@@ -45,6 +151,8 @@ class _SearchScreenState extends State<SearchScreen> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 16),
               child: TextField(
+                key: formkey,
+                controller: searchController,
                 decoration: InputDecoration(
                   prefixIcon: const Icon(Icons.search,
                       color: Color(0xFF9DD6E2), size: 30),
@@ -56,7 +164,7 @@ class _SearchScreenState extends State<SearchScreen> {
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
                       fontFamily: 'Quicksand',
-                      color: Color(0xFF656565)),
+                      color: Color(0xFF537597)),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                     borderSide: const BorderSide(
@@ -70,20 +178,28 @@ class _SearchScreenState extends State<SearchScreen> {
             const SizedBox(
               height: 10,
             ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.fromLTRB(150, 20, 150, 20),
-                  primary: const Color(0xFF77BECE),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12))),
-              child: const Text(
-                'Pesquisar',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
-                    color: Color(0xFFFFFFFF)),
+            Material(
+              elevation: 15,
+              shadowColor: Colors.grey,
+              borderRadius: BorderRadius.circular(15),
+              child: ElevatedButton(
+                onPressed: () {
+                  fetchPSA(searchController.text);
+                  if (formkey.currentState!.validate()) {}
+                },
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.fromLTRB(150, 20, 150, 20),
+                    primary: const Color(0xFF77BECE),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12))),
+                child: const Text(
+                  'Pesquisar',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                      fontSize: 20,
+                      color: Color(0xFFFFFFFF)),
+                ),
               ),
             ),
           ],
